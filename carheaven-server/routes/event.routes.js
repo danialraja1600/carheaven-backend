f7348d4bf7f0200423bc8878aee6f499fffe96cd
drose123456 <acg_danial@hotmail.com>
2023-04-18 13:48:10 +0100

    changed name of route and localhost

--- carheaven-server/routes/event.routes.js
+++ carheaven-server/routes/event.routes.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 const {isAuthenticated} = require("../middleware/jwt.middleware");
 const Event = require("../models/Event.model");
 
-// GET route  ("/")
+// GET route  ("/") 
 // returns a list of events stored in database
-router.get("/", async(req, res, next) => {
+router.get("/getEvents", async(req, res, next) => {
     //route handler is async function to wait for .find() method
     // using async to prevent code from moving to next line until awaited opertion is complete 
     // better/ easier to maintain and to read
@@ -53,6 +53,25 @@ router.get("/:eventId", async(req, res, next) => { // extracting eventId from re
         }
         });
 
+router.get("/myEvents/:userId", isAuthenticated, (req, res, next) => {
+    const { userId } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(userId)) {
+        return res.status(400).json({ message: "User Id is not valid" });
+    }
+    Event.find({creator : userId})
+    .then((myEventsFromDB) => {
+        res.json(myEventsFromDB);
+        })
+    .catch((err) => {
+        console.log("error getting your list of cars", err);
+        res.status(500).json({
+        message: "error getting your list of cars",
+        error: err,
+        });
+    });
+    });
+
     // Creating a new event through POST req to API endpoint
 router.post("/", isAuthenticated, async(req, res, next) => { // before creating event checks if user is authenticated or not
     const event = { // if authenticated, a new event object is created with all these properties
@@ -82,94 +101,94 @@ router.post("/", isAuthenticated, async(req, res, next) => { // before creating 
     });
 
     // PUT route with endpoint /:eventId to update events
-    router.put("/:eventId", isAuthenticated, async (req, res) => { // middleware applied to see if user is authenticated 
-        const { eventId } = req.params; // expecting request parameter 
-        
-        if (!mongoose.Types.ObjectId.isValid(eventId)) { // checking if eventId is valid mongoose objectId
-        return res.status(400).json({ message: "Specified ID is not valid" }); // if not returns error message
-        }
-        
-        try { // if eventId is valid, it finds the event with provided id
-        const event = await Event.findById(eventId);
+router.put("/:eventId", isAuthenticated, async (req, res) => { // middleware applied to see if user is authenticated 
+    const { eventId } = req.params; // expecting request parameter 
+    
+    if (!mongoose.Types.ObjectId.isValid(eventId)) { // checking if eventId is valid mongoose objectId
+    return res.status(400).json({ message: "Specified ID is not valid" }); // if not returns error message
+    }
+    
+    try { // if eventId is valid, it finds the event with provided id
+    const event = await Event.findById(eventId);
 
-        // Check if the event exists
-        if (!event) { //checking if variable is truthy 
-        return res.status(404).json({ message: 'Event not found' });
-        // if not, return error response, status code 404
-      }
+    // Check if the event exists
+    if (!event) { //checking if variable is truthy 
+    return res.status(404).json({ message: 'Event not found' });
+    // if not, return error response, status code 404
+    }
 
-        // Check if the logged-in user is the creator of the event
-        // if statement checking if user making req, is the creator of the event
-        if (event.creator.toString() !== req.payload._id) {
-        // comparing users id with id of event creator after converting it to a string
-        // must convert to string. cant compare a string and an ObjectId type of variable
-        return res.status(403).json({ message: 'Unauthorized' });
-        // if they don't match, return error response with status code 403
-      }
+    // Check if the logged-in user is the creator of the event
+    // if statement checking if user making req, is the creator of the event
+    if (event.creator.toString() !== req.payload._id) {
+    // comparing users id with id of event creator after converting it to a string
+    // must convert to string. cant compare a string and an ObjectId type of variable
+    return res.status(403).json({ message: 'Unauthorized' });
+    // if they don't match, return error response with status code 403
+    }
 
-        // Update "updatedAt" field
-        req.body.updatedAt = Date(); // updating the date with date of update
-        // updating event 
-        const updatedEvent = await Event.findByIdAndUpdate(eventId, req.body, { new: true });
-        // using findByIdAndUpdate method to update the event with new data recieved through req body
-        res.json(updatedEvent); // rerturns updated event in the response
-        }
-        // if any error occurs, catch block is executed.
-        catch (err) { 
-        console.log("error updating event", err);
-        res.status(500).json({ // server responds with error message and status code 500
-        message: "error updating event",
-        error: err,
-        });
-        }
-        });
+    // Update "updatedAt" field
+    req.body.updatedAt = Date(); // updating the date with date of update
+    // updating event 
+    const updatedEvent = await Event.findByIdAndUpdate(eventId, req.body, { new: true });
+    // using findByIdAndUpdate method to update the event with new data recieved through req body
+    res.json(updatedEvent); // rerturns updated event in the response
+    }
+    // if any error occurs, catch block is executed.
+    catch (err) { 
+    console.log("error updating event", err);
+    res.status(500).json({ // server responds with error message and status code 500
+    message: "error updating event",
+    error: err,
+    });
+    }
+    });
 
-        router.delete("/:eventId", isAuthenticated , async(req, res, next) => {
-                //function deletes specific event object by its ID
-                // before creating event checks if user is authenticated or not
+router.delete("/:eventId", isAuthenticated , async(req, res, next) => {
+        //function deletes specific event object by its ID
+        // before creating event checks if user is authenticated or not
 
-            const { eventId } = req.params;
-            
-            if (!mongoose.Types.ObjectId.isValid(eventId)) { // checks if given Id is valid
-            res.status(400).json({ message: "Specified id is not valid" }); //if not rrturns a error message
-            return;
-            }
-            // first must check if given Id is valid
-            //returns a message response if it is not
-            
-            // if it is valid, more auth is carried out
-            // checking if user exists
-            // checking if ids match before allowing user to delete event 
-            try {
-            const event = await Event.findById(eventId);
+    const { eventId } = req.params;
+    
+    if (!mongoose.Types.ObjectId.isValid(eventId)) { // checks if given Id is valid
+    res.status(400).json({ message: "Specified id is not valid" }); //if not rrturns a error message
+    return;
+    }
+    // first must check if given Id is valid
+    //returns a message response if it is not
+    
+    // if it is valid, more auth is carried out
+    // checking if user exists
+    // checking if ids match before allowing user to delete event 
+    try {
+    const event = await Event.findById(eventId);
 
-            // Check if the event exists
-            if (!event) { //checking if variable is truthy 
-            return res.status(404).json({ message: 'Event not found' });
-            // if not, return error response, status code 404
-          }
+    // Check if the event exists
+    if (!event) { //checking if variable is truthy 
+    return res.status(404).json({ message: 'Event not found' });
+    // if not, return error response, status code 404
+    }
 
-            // Check if the logged-in user is the creator of the event
-            // if statement checking if user making req, is the creator of the event
-            if (event.creator.toString() !== req.payload._id) {
-            // comparing users id with id of event creator after converting it to a string
-            // must convert to string. cant compare a string and an ObjectId type of variable
-            return res.status(403).json({ message: 'Unauthorized' });
-            // if they don't match, return error response with status code 403
-          }
-            // // removes the event object from the database if succesful
-            await Event.findByIdAndRemove(eventId);
-            res.json({message: `Event with id ${eventId} is removed successfully.`, 
-            })
-        }
-        catch(err) {
-            console.log("error deleting event", err);
-            res.status(500).json({
-            message: "error deleting event",
-            error: err,
-            });
-            };
-        });
+    // Check if the logged-in user is the creator of the event
+    // if statement checking if user making req, is the creator of the event
+    if (event.creator.toString() !== req.payload._id) {
+    // comparing users id with id of event creator after converting it to a string
+    // must convert to string. cant compare a string and an ObjectId type of variable
+    return res.status(403).json({ message: 'Unauthorized' });
+    // if they don't match, return error response with status code 403
+    }
+    // // removes the event object from the database if succesful
+    await Event.findByIdAndRemove(eventId);
+    res.json({message: `Event with id ${eventId} is removed successfully.`, 
+    })
+}
+catch(err) {
+    console.log("error deleting event", err);
+    res.status(500).json({
+    message: "error deleting event",
+    error: err,
+    });
+    };
+});
 
 
 
